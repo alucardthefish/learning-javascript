a19527917e890f3ea9cc9e7a04642a49bc710e4b
alucardthefish <sergiop18@gmail.com>
2021-03-17 21:21:55 -0500

    Add string modern concatenation

--- scripts/variables.js
+++ scripts/variables.js
@@ -8,4 +8,9 @@ rate = minScore + 2;
 
 score = rate + 3;
 
+// Old way of concatenation
 console.log('score: ' + score + ', rate: ' + rate + ', mininum score: ' + minScore);
+
+// Modern way of concatenation
+const message = `score: ${score}, rate: ${rate}, mininum score: ${minScore}`;
+console.log(message);
